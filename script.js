054e981f4764a6ad1ef965f788514b8eb1f80a1d
GITHUB-Vipul <126069867+GITHUB-Vipul@users.noreply.github.com>
2023-12-08 16:30:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-const extendHex = (shortHex) => {
-  // write your code here
-	let i=shothex.length-3;
-	let ans="#";
-	let isCapital=false;
-	while(i<shortHex.length)
-		{
-			
-			let ascii=shortHex[i].charCodeAt(0);
-			if(ascii>=65&&ascii<=90) isCpaital=true;
-			ans+=(shortHex[i]+shortHex[i]);
-			i++;
-		}
-
-	return isCapital?ans.toUpperCase():ans.toLowerCase();
-};
-
-// Do not change the code below.
-const shortHex = prompt("Enter Short Hex.");
-alert(extendHex(shortHex));
+ const extendHex = (shortHex) => {
+   // write your code here
+    let i=shortHex.length-3;
+    let ans="#";
+    let isCapital=false;
+    while(i<shortHex.length)
+       {
+          
+          let ascii=shortHex[i].charCodeAt(0);
+          if(ascii>=65&&ascii<=90)
+           isCapital=true;
+          ans+=(shortHex[i]+shortHex[i]);
+          i++;
+       }
+ 
+    return isCapital?ans.toUpperCase():ans.toLowerCase();
+ };
+ 
+ // Do not change the code below.
+ const shortHex = prompt("Enter Short Hex.");
+  alert(extendHex(shortHex));
